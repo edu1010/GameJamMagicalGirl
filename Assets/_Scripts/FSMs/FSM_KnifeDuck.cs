7a871c55112e7c1d402d50f35fad4c63172064b4
Eric-Fulla <ericfull007@gmail.com>
2022-03-11 13:02:22 +0100

    intento de que la IA del pato salte. No funciona xd

--- Assets/_Scripts/FSMs/FSM_KnifeDuck.cs
+++ Assets/_Scripts/FSMs/FSM_KnifeDuck.cs
@@ -10,35 +10,59 @@ public class FSM_KnifeDuck : MonoBehaviour
 
     private HealthComponent healthComponent;
     private NavMeshAgent navMeshAgent;
+    private Player_Manager player;
+    private Rigidbody rigidbody3D;
+    private Vector3 playerPos = new Vector3();
 
     public float speed = 10.0f;
+    public float distanceToAttack = 5f;
+    public float distanceToChase = 7f;
+    public float jumpForce = 10f;
+    public float jumpSpeed = 30f;
+    public bool jumping = false;
 
     void Awake()
     {
         healthComponent = GetComponent<HealthComponent>();
         navMeshAgent = GetComponent<NavMeshAgent>();
+        rigidbody3D = GetComponent<Rigidbody>();
     }
 
     void Start()
     {
-        
+        player = Game_Manager.GetGameController().GetPlayer();
     }
 
     void Update()
     {
+        playerPos = player.transform.position;
+
         switch (currentState)
         {
             case State.INITIAL:
                 ChangeState(State.CHASE);
                 break;
             case State.CHASE:
-                //pursue player
+                //Check transitions
+                if (DistanceToPlayer() <= distanceToAttack)
+                    ChangeState(State.ATTACK);
+                else if (jumping)
+                    ChangeState(State.JUMP);
+                //does pursue
                 break;
             case State.ATTACK:
-                //do animation
+                //Check transitions
+                if (DistanceToPlayer() > distanceToChase)
+                    ChangeState(State.CHASE);
+                //Attack
+
                 break;
             case State.JUMP:
+                //Check transitions
+                if (!jumping)
+                    ChangeState(State.CHASE);
                 //jump to next platform
+                
                 break;
             case State.DEAD:
                 //nothing
@@ -52,6 +76,7 @@ public class FSM_KnifeDuck : MonoBehaviour
         switch (currentState)
         {
             case State.CHASE:
+                navMeshAgent.isStopped = true;
                 break;
             case State.ATTACK:
                 break;
@@ -65,11 +90,16 @@ public class FSM_KnifeDuck : MonoBehaviour
         switch (newState)
         {
             case State.CHASE:
-                //navMeshAgent.destination = player;
+                navMeshAgent.isStopped = false;
+                navMeshAgent.destination = playerPos;
                 break;
             case State.ATTACK:
+                print("attacking");
                 break;
             case State.JUMP:
+                navMeshAgent.isStopped = true;
+                rigidbody3D.AddForce(new Vector3(0, 1, 0) * jumpForce);
+                rigidbody3D.velocity = transform.forward * jumpSpeed;
                 break;
             case State.DEAD:
                 enabled = false;
@@ -77,5 +107,10 @@ public class FSM_KnifeDuck : MonoBehaviour
         }
 
         currentState = newState;
+    }
+
+    float DistanceToPlayer()
+    {
+        return (playerPos - gameObject.transform.position).magnitude;
     }
 }
